f144872e7e9bdb1329703c79683ccc2af453d8fd
PDXCrow <piratehans@gmail.com>
2018-01-30 10:18:33 -0800

    separated quote and author vars

--- javascript.js
+++ javascript.js
@@ -1,21 +1,25 @@
+// Array of quotes and authors
 var quotes = [
-  "Just pretend like we're talking until the cops leave. <br> - Creed Bratton",
-  "But on Pretzel Day? I like Pretzel Day. <br>- Stanley Hudson",
-  "I am Beyonce, always. <br>- Michael Scott",
-  "Dwight doesn't blow anything up and I wear a costume. <br>- Jim Halpert",
-  "He said, 'If I'm dead, you guys have been dead for weeks.' <br>- Pam Beesly",
-  "I hooked up with her on February 13th. <br>- Ryan Howard",
-  "I don't talk trash, I talk smack. They're totally different. <br>- Kelly Kapoor",
-  "RYAN STARTED THE FIRE. <br>- Dwight Schrute",
-  "Dwight, you ignorant slut! <br>- Michael Scott",
-  "The worst thing about prison was the dementors. <br>- Michael Scott",
-  "I'm hot, you're hot. Let's get it poppin. <br>- Darryl Philbin",
-  "Do you think doing alcohol is cool? <br>- Michael Scott",
-  "Identity theft is not a joke Jim. Millions of families suffer every year! <br>- Dwight Schrute",
-  "I'm not superstitious, but I am a little stitious. <br>- Michael Scott",
+  ["Just pretend like we're talking until the cops leave.", "Creed Bratton"],
+  ["But on Pretzel Day? I like Pretzel Day.", "Stanley Hudson"],
+  ["I am Beyonce, always.", "Michael Scott"],
+  ["Dwight doesn't blow anything up and I wear a costume.", "Jim Halpert"],
+  ["He said, 'If I'm dead, you guys have been dead for weeks.", "Pam Beesly"],
+  ["I hooked up with her on February 13th.", "Ryan Howard"],
+  ["I don't talk trash, I talk smack. They're totally different.", "Kelly Kapoor"],
+  ["RYAN STARTED THE FIRE!", "Dwight Schrute"],
+  ["Dwight, you ignorant slut!", "Michael Scott"],
+  ["The worst thing about prison was the dementors.", "Michael Scott"],
+  ["I'm hot, you're hot. Let's get it poppin.", "Darryl Philbin"],
+  ["Do you think doing alcohol is cool?", "Michael Scott"],
+  ["Identity theft is not a joke Jim. Millions of families suffer every year!", "Dwight Schrute"],
+  ["I'm not superstitious, but I am a little stitious.", "Michael Scott"],
 ]
 
+
 function newQuote() {
+  // Selects random quote and assigns to quoteDisplay HTML element. Same for author
   var randomNumber = Math.floor(Math.random() * quotes.length);
-  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+  document.getElementById('quoteDisplay').innerHTML = '"' + quotes[randomNumber][0] + '"';
+  document.getElementById('authorDisplay').innerHTML = "- " + quotes[randomNumber][1];
 }
