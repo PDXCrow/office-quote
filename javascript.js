31889f0dc3c08afe32339896c53cd4087f99443a
PDXCrow <piratehans@gmail.com>
2018-01-30 13:10:18 -0800

    hooked up twitter url

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 // Array of quotes and authors
 var quotes = [
+  ["Do you think doing alcohol is cool?", "Michael Scott"],
   ["Just pretend like we're talking until the cops leave.", "Creed Bratton"],
   ["But on Pretzel Day? I like Pretzel Day.", "Stanley Hudson"],
   ["I am Beyonce, always.", "Michael Scott"],
@@ -11,15 +12,20 @@ var quotes = [
   ["Dwight, you ignorant slut!", "Michael Scott"],
   ["The worst thing about prison was the dementors.", "Michael Scott"],
   ["I'm hot, you're hot. Let's get it poppin.", "Darryl Philbin"],
-  ["Do you think doing alcohol is cool?", "Michael Scott"],
   ["Identity theft is not a joke Jim. Millions of families suffer every year!", "Dwight Schrute"],
-  ["I'm not superstitious, but I am a little stitious.", "Michael Scott"],
+  ["I'm not superstitious, but I am a little stitious.", "Michael Scott"]
 ]
 
-
 function newQuote() {
-  // Selects random quote and assigns to quoteDisplay HTML element. Same for author
+  // Selects random quote and assigns to quoteDisplay HTML element. Same for authorDisplay
   var randomNumber = Math.floor(Math.random() * quotes.length);
+  //var tweetQuote = quotes[randomNumber][0].split(' ').join('%20');
+  var tweetQuote = "https://twitter.com/intent/tweet?text=" + '"' + quotes[randomNumber][0] + '"' + " - "
+  + quotes[randomNumber][1];
   document.getElementById('quoteDisplay').innerHTML = '"' + quotes[randomNumber][0] + '"';
   document.getElementById('authorDisplay').innerHTML = "- " + quotes[randomNumber][1];
+  document.getElementById('link').href = tweetQuote;
+  // Build url version of quote to send to twitter
+  //document.getElementById("link").href = "this is a new link";
 }
+//quotes[randomNumber].split(' ').join('%20');
